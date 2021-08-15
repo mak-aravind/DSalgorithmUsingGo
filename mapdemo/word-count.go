39353d0ba5fe1066e9e87c4343c1861a9508f5a5
mak-aravind <aravind.murugaiyan@gmail.com>
2021-08-15 19:24:11 +0530

    extracted 1 as constant

--- mapdemo/word-count.go
+++ mapdemo/word-count.go
@@ -5,12 +5,13 @@ import (
 )
 
 func WordCount(sentence string) map[string]int {
+	const ONE int = 1
 	var wordsMappedToCount map[string]int = make(map[string]int)
 	for _, word := range strings.Fields(sentence) {
 		if count, wordAvailable := wordsMappedToCount[word]; wordAvailable {
-			wordsMappedToCount[word] = count + 1
+			wordsMappedToCount[word] = count + ONE
 		} else {
-			wordsMappedToCount[word] = 1
+			wordsMappedToCount[word] = ONE
 		}
 	}
 	return wordsMappedToCount
