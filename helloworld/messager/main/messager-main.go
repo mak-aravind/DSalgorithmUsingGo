264be0fbf10472043312a8fd5b674bb4c9b312be
mak-aravind <aravind.murugaiyan@gmail.com>
2021-08-17 19:49:16 +0530

    folder re-organized

---
+++ helloworld/messager/main/messager-main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	"example.com/go-demo/helloworld/messager"
+)
+
+func main() {
+	fmt.Println(messager.GetMessage())
+}
---
+++ helloworld/messager/messager-publisher.go
@@ -0,0 +1,13 @@
+package messager
+
+func GetMessage() string {
+	return "Hello World"
+}
+
+func GetHappyMessage() string {
+	return "I am happy"
+}
+
+func GetSadMessage() string {
+	return "I am sad"
+}
