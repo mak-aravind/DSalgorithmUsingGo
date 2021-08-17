ce7aafea0368e4866b06335eb3cdd0c4d6245422
mak-aravind <aravind.murugaiyan@gmail.com>
2021-08-17 18:55:15 +0530

    delete solve folder. its redundant

--- map-problems/twosum/two-sum.go
+++ map-problems/twosum/two-sum.go
@@ -2,14 +2,14 @@ package twosum
 
 func GetAddendIndics(target int, nums []int) (found bool, indices [2]int) {
 	addendLookUpTable := make(map[int]int)
-	for addendIndex, addendInEnquiry := range nums {
-		if complementIndex, complementAvailable := addendLookUpTable[target-addendInEnquiry]; complementAvailable {
+	for enquiredAddendIndex, addendToEnquire := range nums {
+		if complementIndex, complementAvailable := addendLookUpTable[target-addendToEnquire]; complementAvailable {
 			indices[0] = complementIndex
-			indices[1] = addendIndex
+			indices[1] = enquiredAddendIndex
 			found = true
 			return
 		}
-		addendLookUpTable[addendInEnquiry] = addendIndex
+		addendLookUpTable[addendToEnquire] = enquiredAddendIndex
 	}
 	return
 }
